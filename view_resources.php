2f779aa4000fbead5eadee8554b7a57977b762da
elw-arduino <minecraftisawsome007+github@gmail.com>
2018-08-21 19:24:30 -0400

    More Borring Resource Stuff

--- view_resources.php
+++ view_resources.php
@@ -9,7 +9,7 @@ if(substr_count($dir_name, CATSDIR_RESOURCES) == 0){
 $dir = new DirectoryIterator($dir_name);
 foreach ($dir as $fileinfo) {
     if (!$fileinfo->isDot()) {
-        $s .= "<a href='".str_replace(" ", "%20", $fileinfo->getFilename())."'>".$fileinfo->getFilename()."</a><br />";
+        $s .= "<a href='".$dir_name.str_replace(" ", "%20", $fileinfo->getFilename())."'>".$fileinfo->getFilename()."</a><br />";
         
     }
 }
