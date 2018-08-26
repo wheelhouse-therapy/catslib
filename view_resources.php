b5c9fed5ea1aaafaf0b01d8c0af82a1e68223a88
elw-arduino <minecraftisawsome007+github@gmail.com>
2018-08-25 20:16:02 -0400

    Added Message for empty directory

--- view_resources.php
+++ view_resources.php
@@ -7,6 +7,10 @@ if(substr_count($dir_name, CATSDIR_RESOURCES) == 0){
     $dir_name = CATSDIR_RESOURCES.$dir_name;
 }
 $dir = new DirectoryIterator($dir_name);
+if(iterator_count($dir) == 0){
+    $s .= "<h2> No files in directory</h2>";
+    return;
+}
 foreach ($dir as $fileinfo) {
     if (!$fileinfo->isDot()) {
         $s .= "<a href='".$dir_name.str_replace(" ", "%20", $fileinfo->getFilename())."'>".$fileinfo->getFilename()."</a><br />";
