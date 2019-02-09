ff2a4da92c6fbca831df5f19cc9865ae5446957a
elw-arduino <minecraftisawsome007+github@gmail.com>
2019-02-09 13:01:48 -0500

    Styled Print stuff

--- assessments.php
+++ assessments.php
@@ -1081,7 +1081,7 @@ function AssessmentsScore( SEEDAppConsole $oApp )
             if($sRight){
                 $s .= "<script src='w/js/printme/jquery-printme.js'></script>"
                     ."<div style='padding:inherit;display:inline'>"
-                        ."<button onclick='$(\"#assessment\").printMe();'>Print Assessment</button>"
+                        ."<button style='background: url(".CATSDIR_IMG."Print.png) 0px/24px no-repeat; width: 24px; height: 24px;border:  none;' data-tooltip='Print Assessment' onclick='$(\"#assessment\").printMe({ \"path\": [\"w/css/spmChart.css\"]});'></button>"
                     ."</div>";
             }
             $s .= "</div><div class='container-fluid'><div class='row'>"
