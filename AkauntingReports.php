bc155f3bc3d87891755946c5a44282f03d191cfa
elw-arduino <minecraftisawsome007+github@gmail.com>
2019-04-21 16:18:05 -0400

    Added Drop down to select clinic for eligable users

--- AkauntingReports.php
+++ AkauntingReports.php
@@ -6,6 +6,7 @@ class AkauntingReports
     private $oAppAk = null;     // SEEDAppDB
     private $akDb;
     private $akTablePrefix;
+    private $clinics;
 
 
     function __construct( SEEDAppConsole $oApp )
@@ -13,6 +14,7 @@ class AkauntingReports
         global $config_KFDB;
 
         $this->oApp = $oApp;
+        $this->clinics = new Clinics($this->oApp);
 
         if( isset($config_KFDB['akaunting']) ) {
             $this->oAppAk = new SEEDAppDB( $config_KFDB['akaunting'] );
@@ -28,11 +30,18 @@ class AkauntingReports
     {
         $sOrderBy = @$raParms['sort'] ? " ORDER BY {$raParms['sort']} " : "";
 
+        $cid =(new ClinicsDB($this->oApp->kfdb))->GetClinic(@$raParms['clinic']?:$this->clinics->GetCurrentClinic())->Value("akaunting_company");
+        
+        if(!$cid){
+            $this->oApp->oC->AddErrMsg("Clinic does not have an accounting ID set");
+            return( FALSE );
+        }
+        
         $sql =
             "select A.account_id,A.entry_type,A.debit as d,A.credit as c,LEFT(A.issued_at,10) as date, "
                   ."B.company_id as company_id, B.type_id as type_id, B.code as code, B.name as name "
             ."from {$this->akTablePrefix}_double_entry_ledger A, {$this->akTablePrefix}_double_entry_accounts B "
-            ."where A.account_id=B.id"
+            ."where A.account_id=B.id AND company_id=$cid"
             .$sOrderBy;
 
         $raRows = $this->oAppAk->kfdb->QueryRowsRA( $sql );
@@ -44,7 +53,7 @@ class AkauntingReports
     {
         $raParms = [];
 
-        if( ($p = SEEDInput_Str('sort')) ) {
+        if( ($p = $this->oApp->sess->SmartGPC('Akaunting_sort')) ) {
             switch( $p ) {
                 case 'date':
                     $raParms['sort'] = 'date,name,d,c';
@@ -55,6 +64,9 @@ class AkauntingReports
                 default:
                     break;
             }
+        }
+        if( ($p = $this->oApp->sess->SmartGPC('Akaunting_clinic'))){
+            $raParms['clinic'] = $p;
         }
 
         return( $raParms );
@@ -68,9 +80,24 @@ class AkauntingReports
 
         $raRows = $this->GetLedgerRA( $this->LedgerParmsFromRequest() );
 
+        if($clinics = $this->clinics->getClinicsWithAkaunting()){
+            $clinic = $this->oApp->sess->SmartGPC('Akaunting_clinic', array($this->clinics->GetCurrentClinic()));
+            $clinicsDB = new ClinicsDB($this->oApp->kfdb);
+            $sForm = "<form style='display:inline' id='companyForm'><select name='Akaunting_clinic' onChange=\"document.getElementById('companyForm').submit()\">";
+            foreach($clinics as $option){
+                $raData = $clinicsDB->GetClinic($option);
+                if($option == $clinic){
+                    $sForm = SEEDCore_ArrayExpand($raData, "<option selected value='[[akaunting_company]]'>[[clinic_name]]</option>");
+                }
+                else{
+                    $sForm = SEEDCore_ArrayExpand($raData, "<option value='[[akaunting_company]]'>[[clinic_name]]</option>");
+                }
+            }
+        }
+        
         $s = "<table cellpadding='10' border='1'>"
-            ."<tr><th>Company</th><th><a href='{$_SERVER['PHP_SELF']}?sort=date'>Date</a></th><th><a href='{$_SERVER['PHP_SELF']}?sort=name'>Account</a></th><th>Debit</th><th>Credit</th></tr>"
-            .SEEDCore_ArrayExpandRows( $raRows, "<tr><td>[[company_id]]</td><td>[[date]]</td><td>[[code]] : [[name]]</td><td> [[d]]</td><td> [[c]]</tr>" )
+            ."<tr><th><a href='{$_SERVER['PHP_SELF']}?Akaunting_sort=date'>Date</a></th><th><a href='{$_SERVER['PHP_SELF']}?Akaunting_sort=name'>Account</a></th><th>Debit</th><th>Credit</th></tr>"
+            .($raRows?SEEDCore_ArrayExpandRows( $raRows, "<tr><td>[[date]]</td><td>[[code]] : [[name]]</td><td> [[d]]</td><td> [[c]]</tr>"):"Could not get Accounting Data" )
             ."</table>";
 
 
