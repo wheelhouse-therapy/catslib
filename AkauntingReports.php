7659644bc810349135aa785223f673a2f85e213f
bobwildfong <bob@seeds.ca>
2019-04-21 14:50:41 -0400

    write Akaunting to spreadsheet

--- AkauntingReports.php
+++ AkauntingReports.php
@@ -68,7 +68,17 @@ class AkauntingReports
 
         $raRows = $this->GetLedgerRA( $this->LedgerParmsFromRequest() );
 
-        $s = "<table cellpadding='10' border='1'>"
+        $sCurrParms = "sort=".SEEDInput_Str('sort');
+
+
+        $s .= "<div style='clear:both;float:right; border:1px solid #aaa;border-radius:5px;padding:10px'>"
+                 ."<a href='jx.php?cmd=therapist-akaunting-xlsx&$sCurrParms'><button>Download</button></a>"
+                 ."&nbsp;&nbsp;&nbsp;&nbsp;"
+                 ."<img src='".W_CORE_URL."img/icons/xls.png' height='30'/>"
+                 ."&nbsp;&nbsp;&nbsp;&nbsp;"
+             ."</div>";
+
+        $s .= "<table cellpadding='10' border='1'>"
             ."<tr><th>Company</th><th><a href='{$_SERVER['PHP_SELF']}?sort=date'>Date</a></th><th><a href='{$_SERVER['PHP_SELF']}?sort=name'>Account</a></th><th>Debit</th><th>Credit</th></tr>"
             .SEEDCore_ArrayExpandRows( $raRows, "<tr><td>[[company_id]]</td><td>[[date]]</td><td>[[code]] : [[name]]</td><td> [[d]]</td><td> [[c]]</tr>" )
             ."</table>";
@@ -93,8 +103,105 @@ function AkauntingReport( SEEDAppConsole $oApp )
 
 function AkauntingReport_OutputXLSX( SEEDAppConsole $oApp )
 {
+    $o = new AkauntingReports( $oApp );
+
+    $oXls = new AkauntingReportSpreadsheet( $oApp );
+    $oXls->OutputXLSX( $o->LedgerParmsFromRequest() );
+}
 
 
+class AkauntingReportSpreadsheet
+{
+    private $oApp;
+    private $oPeopleDB;
+    private $clinics;
+
+    function __construct( SEEDAppConsole $oApp )
+    {
+        $this->oApp = $oApp;
+        $this->oPeopleDB = new PeopleDB( $this->oApp );
+        $this->clinics = new Clinics($oApp);
+        $this->clinics->GetCurrentClinic();
+    }
+
+    function OutputXLSX( $raParms = [] )
+    {
+        // Initialize the spreadsheet with three sheets (one is created by default)
+        $oXls = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+        $oXls->createSheet();
+        $oXls->createSheet();
+
+        // Set document properties
+        $oXls->getProperties()->setCreator('Collaborative Approach Therapy Services')
+            ->setLastModifiedBy('CATS')
+            ->setTitle('Clients / Staff / Providers')
+            ->setSubject('Clients / Staff / Providers')
+            ->setDescription('Spreadsheet containing Akaunting details')
+            ->setKeywords('')
+            ->setCategory('CATS Akaunting');
+
+        $filename = "CATS Akaunting.xlsx";
+
+        $o = new AkauntingReports( $this->oApp );
+        $raRows = $o->GetLedgerRA( $raParms );
+
+        $raCols = array(
+            'date'   => 'Date',
+            'name'    => 'Last name',
+            'd'      => 'Debit',
+            'c'         => 'Credit',
+        );
+
+        $this->storeSheet( $oXls, 0, "Akaunting", $raRows, $raCols );
+
+        // Set active sheet index to the first sheet, so Excel opens this as the first sheet
+        $oXls->setActiveSheetIndex(0);
+
+        // Redirect output to a client’s web browser (Xlsx)
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header("Content-Disposition: attachment;filename=\"$filename\"");
+        header('Cache-Control: max-age=0');
+        // If you're serving to IE 9, then the following may be needed
+        header('Cache-Control: max-age=1');
+
+        // If you're serving to IE over SSL, then the following may be needed
+        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+        header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
+        header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+        header('Pragma: public'); // HTTP/1.0
+
+        $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($oXls, 'Xlsx');
+        $writer->save('php://output');
+    }
+
+    private function storeSheet( $oXls, $iSheet, $sSheetName, $raRows, $raCols )
+    {
+//        $c = 'A';
+ //       foreach( $raCols as $ra ) {
+  //          var_dump($c);
+   //     }exit;
+
+        $oSheet = $oXls->setActiveSheetIndex( $iSheet );
+        $oSheet->setTitle( $sSheetName );
+
+        // Set the headers in row 1
+        $c = 'A';
+        foreach( $raCols as $dbfield => $label ) {
+            $oSheet->setCellValue($c.'1', $label );
+            $c = chr(ord($c)+1);    // Change A to B, B to C, etc
+        }
+
+        // Put the data starting at row 2
+        $row = 2;
+        foreach( $raRows as $ra ) {
+            $col = 'A';
+            foreach( $raCols as $dbfield => $label ) {
+                $oSheet->setCellValue($col.$row, $ra[$dbfield] );
+                $col = chr(ord($col)+1);    // Change A to B, B to C, etc
+            }
+            ++$row;
+        }
+    }
 }
 
 ?>
