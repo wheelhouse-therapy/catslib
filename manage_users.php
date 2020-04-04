d8cdbf4cb5a3d9ddfe3cfc006f40735ea34c59b6
elw-arduino <minecraftisawsome007+github@gmail.com>
2020-04-04 10:45:47 -0400

    Started work on new manage users system

---
+++ manage_users.php
@@ -0,0 +1,15 @@
+<?php
+
+class ManageUsers {
+    
+    private $oApp;
+    
+    public function __construct(SEEDAppConsole $oApp){
+        $this->oApp = $oApp;
+    }
+    
+    public function manageUser(int $uid){
+        
+    }
+    
+}
