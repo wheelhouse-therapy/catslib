7e3d4f93f344b0f7b9d5d0fccd0cd74070eef2d9
elw-arduino <minecraftisawsome007+github@gmail.com>
2020-10-09 09:19:54 -0400

    Added Comments

--- tutorial.php
+++ tutorial.php
@@ -4,6 +4,7 @@
 //Code in this file is Property of Eric Wildfong and Licensenced to the CATS Backend software.
 /**
  * Class responsible for handling showing users a tutorial if they havent already sceen it.
+ * This class is self initializing meaning init() does not need to be called first
  * @author Eric Wildfong
  * @version 1.0
  * @copyright Eric Wildfong 2020
@@ -216,6 +217,8 @@ TutorialScript;
             return;
         }
         
+        // Get instances of all the tutorial classes and index them by their screen.
+        // This index is used by hasTutorial() to determine if a tutorial for a screen exists
         $classes = get_declared_classes();
         foreach($classes as $klass) {
             $reflect = new ReflectionClass($klass);
