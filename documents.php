b9cd933bc89dcf7aaf2f92228d027bee6724202b
elw-arduino <minecraftisawsome007+github@gmail.com>
2020-10-26 10:51:13 -0400

    more debugging

--- documents.php
+++ documents.php
@@ -398,7 +398,7 @@ class ResourceManager{
                 $filename = FilingCabinet::GetDirInfo($filename)['name'];
             }
             $s .= "<a href='javascript:void(0)' onclick='toggleDisplay(\"".addslashes($this->getPathRelativeTo($fileinfo->getRealPath(),CATSDIR_RESOURCES))."\")'>".$filename."</a><br />";
-            if(CATS_SYSADMIN){
+            if(CATS_SYSADMIN && $fileinfo->isFile()){
                 var_dump(addslashes($this->getPathRelativeTo($fileinfo->getRealPath(),CATSDIR_RESOURCES)));
             }
             $s .= "<div class='[style]' id='".addslashes($this->getPathRelativeTo($fileinfo->getRealPath(),CATSDIR_RESOURCES))."' style='".(in_array(addslashes($this->getPathRelativeTo($fileinfo->getRealPath(),CATSDIR_RESOURCES)), $this->openTrees)?"":"display:none;")." width: [width];'>";
