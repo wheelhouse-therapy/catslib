293ecca16aadf0e316dc34166dc27303be9ac8d3
bobwildfong <bob@seeds.ca>
2020-11-21 13:50:28 -0500

    add config parms CATSDIR_URL*

--- _config.php
+++ _config.php
@@ -17,11 +17,17 @@ if( CATS_DEBUG ) {
     ini_set('html_errors', 1);
 }
 
-/* CATSDIR is the location of the cats root directory. If you're doing something weird like running cats from
+/* CATSDIR is the location of the cats root directory. If you're doing something weird like running cats
  * by including cats/index.php from another directory, define CATSDIR relative to that place before your include.
+ *
+ * CATSDIR_URL is used for <a href>, <img src>, <script src>, etc links i.e. where the browser will fetch a file.
+ * Often this is the same as CATSDIR but you could configure CATSDIR with an absolute filesystem dir which wouldn't work for http links.
+ * Hint: if you set an absolute CATSDIR you probably want "./" for CATSDIR_URL
+ *
  * Otherwise, this default will make things work for the normal case.
  */
-if( !defined("CATSDIR") ) { define( "CATSDIR", "./" ); }
+if( !defined("CATSDIR") )     { define( "CATSDIR", "./" ); }            // filesystem location of root dir
+if( !defined("CATSDIR_URL") ) { define( "CATSDIR_URL", CATSDIR ); }     // http link location of root dir
 
 // SEEDROOT has to be configured; seedConfig.php should be able to find everything from there. If not, predefine what it can't find.
 if( !file_exists(SEEDROOT."seedConfig.php") ) die( "SEEDROOT is not correct: ".SEEDROOT );
@@ -55,7 +61,9 @@ require_once 'manage_users.php';
 if( !defined("CATSDIR_IMG") ) { define( "CATSDIR_IMG", CATSDIR."w/img/" ); }
 if( !defined("CATSDIR_JS") ) { define( "CATSDIR_JS", CATSDIR."w/js/" ); }
 if( !defined("CATSDIR_CSS") ) { define( "CATSDIR_CSS", CATSDIR."w/css/" ); }
-if( !defined("CATSDIR_RESOURCES") ) { define( "CATSDIR_RESOURCES", CATSDIR."resources/" ); }
+if( !defined("CATSDIR_RESOURCES") )     { define( "CATSDIR_RESOURCES", CATSDIR."resources/" ); }            // filesystem
+if( !defined("CATSDIR_URL_RESOURCES") ) { define( "CATSDIR_URL_RESOURCES", CATSDIR_URL."resources/" ); }    // http
+
 if( !defined("CATSDIR_DOCUMENTATION")){ define( "CATSDIR_DOCUMENTATION", CATSDIR."w/documentation/");}
 if( !defined("CATSDIR_AKAUNTING")){ define( "CATSDIR_AKAUNTING", CATSDIR."w/akaunting/");}
 if( !defined("CATSDIR_FONTS")){ define( "CATSDIR_FONTS", CATSDIR."w/fonts/");}
