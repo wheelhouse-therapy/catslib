0b97257511c4d9391e7acfd39e2377fad0f7a152
elw-arduino <minecraftisawsome007+github@gmail.com>
2020-12-26 17:47:22 -0500

    Fixed Error Reviewing Resources

--- FilingCabinet/FilingCabinetReview.php
+++ FilingCabinet/FilingCabinetReview.php
@@ -184,9 +184,9 @@ function disable(rrid,event){
             if( rename($this->dirPending.$file, CATSDIR_RESOURCES.FilingCabinet::GetDirInfo($dir)['directory'].$subdir.$file)){
                 if(!ResourceRecord::GetRecordFromPath($this->oApp, $cabinet, $dir, $file, $subdir)){
                     $s .= "<div class='alert alert-success'> File ".$file." has been accepted as a resource</div>";
-                    $oRR->setCabinet( $sCabinet );
+                    $oRR->setCabinet( $cabinet );
                     $oRR->setDirectory($dir);
-                    $oRR->setSubDirectory(@$ra[1]?: "");
+                    $oRR->setSubDirectory($subdir);
                     if(!$oRR->StoreRecord()){
                         $s .= "<div class='alert alert-danger'>Unable to update the index. Contact a System Administrator Immediately (Code 504-{$oRR->getID()})</div>";
                     }
