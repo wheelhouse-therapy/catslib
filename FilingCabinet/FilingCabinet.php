3b4df0057fa8b6378e546d97952d8e4b5c72a468
elw-arduino <minecraftisawsome007+github@gmail.com>
2021-01-01 17:55:29 -0500

    Increaded groups to 8 (Weekly

--- FilingCabinet/FilingCabinet.php
+++ FilingCabinet/FilingCabinet.php
@@ -354,7 +354,7 @@ class ResourceRecord {
     // Cutoff for resources to be considered "new"
     private const NEWNESS_CUTOFF = 60;
     // How many "groups" of "new" resources there are, depicted by different "badges" in the filing cabinet
-    private const NEWNESS_GROUPS = 4;
+    private const NEWNESS_GROUPS = 8;
 
     private const TAG_SEPERATOR = "\t";
 
